9bc0877411def061b14ae2a5a7e09c8a8ceb3cc7
Linus <Linus.ostren@gmail.com>
2012-10-19 14:35:11 +0200

    manual.c updated

--- manual.c
+++ manual.c
@@ -1,7 +1,13 @@
+#ifndef __manual__
+#define __manual__
+
 #include <ctype.h>
 #include <stdio.h>
+#include "imalloc.h"
+#include "priv_imalloc.h"
+
 int typeReader(char* input) {
-  int multiply = 0, result = 0, i = 0, size;
+    int multiply = 0, result = 0, i = 0, size;
     while (input[i]) {
         if (isdigit(input[i])) {
             multiply *= 10;
@@ -49,8 +55,21 @@ int typeReader(char* input) {
     return result;
     }
 
+unsigned int avail(Memory mem) {
+    Chunk c = mem->data;
+    int avail = 0;
+    for (; c; c = c->next) {
+        if (c->free) {
+            avail += c->size;
+            }
+        }
+    return avail;
+    }
+
 int main() {
     int b = typeReader("20i\0");
     printf("answer: %d\n",b);
     return 0;
     }
+
+#endif
