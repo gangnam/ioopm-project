528689a3b3a891616018f5820962240837f29999
Robin Nyman <astral.ice@gmail.com>
2012-10-19 14:40:32 +0200

    We are awesome.

--- manual.c
+++ manual.c
@@ -1,57 +1,55 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int typeReader(char* input) {
-  int multiply = 0;
-  int result = 0;
-  while (input)
-    {
-      if (isdigit(*input))
-	{
-	  multiply *= 10;
-	  multiply += *input;
-	  input++;
-	}
-      if (multiply == 0) 
-	{
-	  multiply = 1;
-	}
-      if  (*input == '*')
-	{
-	  result += multiply * sizeof(void*);
-	  multiply = 0;
-	} 
-      else if (*input == 'i')
-	{
-	  result += multiply * sizeof(int);
-	  multiply = 0;
+    int multiply = 0;
+    int result = 0;
+    int i = 0;
+
+    while (input[i]) {
+        if (isdigit(input[i])) {
+            multiply *= 10;
+            multiply += (input[i] - 48);
+            }
+        else {
+            if (multiply == 0) {
+                multiply = 1;
+                }
+            if  (input[i] == '*') {
+                result += (multiply * sizeof(void*));
+                multiply = 0;
+                }
+            else if (input[i] == 'i') {
+                result += multiply * sizeof(int);
+                multiply = 0;
+                }
+            else if (input[i] == 'l') {
+                result += multiply * sizeof(long);
+                multiply = 0;
+                }
+            else if (input[i] == 'f') {
+                result += multiply * sizeof(float);
+                multiply = 0;
+                }
+            else if (input[i] == 'd') {
+                result += multiply * sizeof(double);
+                multiply = 0;
+                }
+            else {
+                result += (multiply * sizeof(char));
+                multiply = 0;
+                }
+            }
+        i++;
+        }
+    return result;
     }
-      else if (*input == 'l')
-	{
-	  result += multiply * sizeof(long);
-	  multiply = 0;
-	}
-      else if (*input == 'f')
-	{
-	  result += multiply * sizeof(float);
-      multiply = 0;
-    }
-  else if (*input == 'd')
-    {
-      result += multiply * sizeof(double);
-      multiply = 0;
-    }
-  else
-    {
-      result += multiply * sizeof(char);
-      multiply = 0;
-    }
-  input++;
-  multiply = 0;
-}
-  return result;
-}
 
 int main() {
-  typeReader("*");
-  return 0;
-}
+    char *test = "3**5d\0";
+    int p = typeReader(test);
+    printf("%d\n", p);
+    return 0;
+    }
