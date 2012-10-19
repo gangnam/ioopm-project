89f69cc160e74acd4689255b6227ad9296d1c844
Linus <Linus.ostren@gmail.com>
2012-10-19 14:04:43 +0200

    Nu är typereader fin

--- manual.c
+++ manual.c
@@ -1,57 +1,56 @@
 #include <ctype.h>
 #include <stdio.h>
 int typeReader(char* input) {
-  int multiply = 0;
-  int result = 0;
-  while (input)
-    {
-      if (isdigit(*input))
-	{
-	  multiply *= 10;
-	  multiply += *input;
-	  input++;
-	}
-      if (multiply == 0) 
-	{
-	  multiply = 1;
-	}
-      if  (*input == '*')
-	{
-	  result += multiply * sizeof(void*);
-	  multiply = 0;
-	} 
-      else if (*input == 'i')
-	{
-	  result += multiply * sizeof(int);
-	  multiply = 0;
+  int multiply = 0, result = 0, i = 0, size;
+    while (input[i]) {
+        if (isdigit(input[i])) {
+            multiply *= 10;
+            multiply += (input[i] - 48);
+            }
+        else {
+            if (multiply == 0) {
+                multiply = 1;
+                }
+
+            switch (input[i]) {
+
+                case '*' : {
+                    size = sizeof(void*);
+                    break;
+                    }
+                case 'i' : {
+                    size = sizeof(int);
+                    break;
+                    }
+                case 'l' : {
+                    size = sizeof(long);
+                    break;
+                    }
+                case 'f' : {
+                    size = sizeof(float);
+                    break;
+                    }
+                case 'd': {
+                    size = sizeof(double);
+                    break;
+                    }
+                default: {
+                    size = sizeof(char);
+                    break;
+                    }
+                }
+
+            result += multiply * size;
+            multiply = 0;
+
+            }
+        i++;
+        }
+    return result;
     }
-      else if (*input == 'l')
-	{
-	  result += multiply * sizeof(long);
-	  multiply = 0;
-	}
-      else if (*input == 'f')
-	{
-	  result += multiply * sizeof(float);
-      multiply = 0;
-    }
-  else if (*input == 'd')
-    {
-      result += multiply * sizeof(double);
-      multiply = 0;
-    }
-  else
-    {
-      result += multiply * sizeof(char);
-      multiply = 0;
-    }
-  input++;
-  multiply = 0;
-}
-  return result;
-}
 
 int main() {
-  typeReader("*");
-  return 0;
-}
+    int b = typeReader("20i\0");
+    printf("answer: %d\n",b);
+    return 0;
+    }
