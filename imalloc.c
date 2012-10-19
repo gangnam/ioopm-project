83cbfde8d88bc852592cbf4a6d334aa205c6fbd4
Robin Nyman <astral.ice@gmail.com>
2012-10-19 12:11:17 +0200

    Uppdaterade filer.

--- imalloc.c
+++ imalloc.c
@@ -1,55 +1,63 @@
 typedef struct chunk *Chunk;
 
-struct chunk {
-	void* start; // pointer to start of memory
-	unsigned size; // how big is this chunk
-	Chunk next; // pointer to next chunk
-	bool free; // true if the chunk is free, else false
+struct chunk
+{
+    void* start; // pointer to start of memory
+    unsigned size; // how big is this chunk
+    Chunk next; // pointer to next chunk
+    bool free; // true if the chunk is free, else false
 };
 
-Chunk init(unsigned int bytes) {
-	char *memory = (char*) malloc(bytes);
-	Chunk H = (Chunk) malloc(sizeof(chunk));
-	H->start = (void*) memory;
-	H->size = bytes;
-	H->next = NULL;
-	H->free = 1;
-	while (bytes) memory[--bytes] = 0;
-	return H;
+Chunk init(unsigned int bytes)
+{
+    char *memory = (char*) malloc(bytes);
+    Chunk H = (Chunk) malloc(sizeof(chunk));
+    H->start = (void*) memory;
+    H->size = bytes;
+    H->next = NULL;
+    H->free = 1;
+    while (bytes) memory[--bytes] = 0;
+    return H;
 }
 
-void *smalloc(Memory mem, chunkSize siz) {
+void *alloc(Memory mem, chunkSize siz)
+{
 // Back up one pointer in memory to access the first chunk Chunk c = (Chunk) ((char*) mem)-sizeof(void*);
-	while (!fits(c, bytes)) c = c->next;
-	if (c) {
-		return split(c, bytes);
-	}
-	else {
-		return NULL;
-	}
+    while (!fits(c, bytes)) c = c->next;
+    if (c)
+        {
+            return split(c, bytes);
+        }
+    else
+        {
+            return NULL;
+        }
 }
-unsigned int avail(Memory mem) {
+unsigned int avail(Memory mem)
+{
 // Back up one pointer in memory to access the first chunk Chunk c = (Chunk) ((char*) mem)-sizeof(void*);
-	int avail = 0;
-	for (; c; c = c->next)
-	if (c->free) avail += c->size;
-	return avail;
+    int avail = 0;
+    for (; c; c = c->next)
+        if (c->free) avail += c->size;
+    return avail;
 }
 
-struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
+struct style *iMalloc(unsigned int memsiz, unsigned int flags)
+{
 // Ignoring free list ordering in this simple example
-	if (flags & MANUAL) {
-    // Allocate space for the struct of functions and metadata
-		struct private_manual *mgr = malloc(sizeof(private_manual));
+    if (flags & MANUAL)
+        {
+            // Allocate space for the struct of functions and metadata
+            struct private_manual *mgr = malloc(sizeof(private_manual));
 
-		// Allocate the space that the memory manager will manage
-	    mgr->data = init(memsiz);
-	    // Install the functions
-		mgr->functions->alloc = smalloc;
-		mgr->functions->avail = avail;
-		mgr->functions->free = sfree;
-		return &(manual->functions);
-	}
-   // Implement all other cases
-	return NULL;
+            // Allocate the space that the memory manager will manage
+            mgr->data = init(memsiz);
+            // Install the functions
+            mgr->functions->alloc = alloc;
+            mgr->functions->avail = avail;
+            mgr->functions->free = sfree;
+            return &(manual->functions);
+        }
+    // Implement all other cases
+    return NULL;
 }
