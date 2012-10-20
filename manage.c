321298729b78e96801fd4f23659ef21ab505b04b
Leila <leila.benhalima2@gmail.com>
2012-10-20 16:18:10 +0200

    manage

---
+++ manage.c
@@ -0,0 +1,12 @@
+
+
+//ska printa ut alla rotpekare
+void printPtr(void *ptr, void *ignore) {
+  printf("%p\n", ptr);
+}
+
+//traverserar/skannar stacken inom en viss adresssrymd h, där f kan vara
+// t.ex. printPtr
+traverseStack(AddresSpace h, MarkFun f, void *p) {
+}
+
