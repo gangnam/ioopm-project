d9a60eff813e475c5df3adc5cfbe616686197718
Jesper Andersson <juppeduppe@gmail.com>
2012-10-23 14:50:13 +0200

    hej

---
+++ manage.h
@@ -0,0 +1,11 @@
+#ifndef __manage_h
+#define __manage_h
+#include "priv_imalloc.h"
+
+void GC(Chunk c);
+void setZero (Chunk c); 
+void traverseStack(AdressSpace h, Markfun f, void *p);
+void freeObj (chunk c);
+void printPtr(void *ptr, void *ignore);
+
+#endif
