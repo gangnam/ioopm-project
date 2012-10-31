690f2a475a105c7f784c5bf2811fd0d229a4b9e2
Jesper Andersson <juppeduppe@gmail.com>
2012-10-31 10:49:21 +0100

    fix

--- Garbage.h
+++ Garbage.h
@@ -1,12 +1,7 @@
 #ifndef __manage_h
 #define __manage_h
-#include "priv_imalloc.h"
 
-typedef char *RawPtr;
-typedef struct addressspace{
-    RawPtr start;
-    RawPtr end;
-    } addressspace, *AddressSpace;
+
 
 
 void *typeReader(Memory mem, char *input);
@@ -16,7 +11,7 @@ void collectGarbage(Memory mem);
 
 // Kollar om rotpekaren i givna adressrymden pekar in i en chunk och markerar
 // den i sådana fall
-void (*MarkFun)(void *ptr, void *data);
+
 void mf(void *ptr, void *data);
 
 // Frigör alla objekt vars markbitar fortfarande är satta till 0
