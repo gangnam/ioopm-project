6eb10040b31776e307f1cd20083cd85248c0302e
Nadia <roning.nadia@gmail.com>
2012-10-31 21:42:45 +0100

    koddoc

--- rc.h
+++ rc.h
@@ -2,9 +2,13 @@
 #define __rc_h_
 #include "imalloc.h"
 
+/*ökar referens pekaren och retunerar referenspekaren till chunken*/
 unsigned int increaseReferenceCounter (void *ptr);
 
+/*Tar bort en pekare och när chunken inte har kvar några referenser 
+så läggs chunken i freelistan*/
 unsigned int decreaseReferenceCounter (Memory mem, void *ptr);
 
+/*retunerar referenspekare till en chunk */
 unsigned int returnReferenceCounter (void *ptr);
 #endif
