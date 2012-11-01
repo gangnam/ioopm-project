a1de8efdf03b2867e45de5245dda2b02113e94f1
Linus <Linus.ostren@gmail.com>
2012-11-01 13:40:07 +0100

    unittests igen

--- unittests.c
+++ unittests.c
@@ -33,7 +33,8 @@ int clean_suite_gc(void) {
     return 0;
 }
 
-void testMANUAL_ASCENDING(void) {
+void testMANUAL_ASCENDING(void) 
+{
   Manual mem = (Manual) iMalloc(1 Mb, MANUAL + ASCENDING_SIZE);
   CU_ASSERT(mem->free == ascending_free);
   CU_ASSERT(mem->avail == avail);
@@ -42,11 +43,11 @@ void testMANUAL_ASCENDING(void) {
   Chunk c = temp->data;
   CU_ASSERT(c->size == (1 Mb - sizeof(chunk)));
   CU_ASSERT(c->free == 1);
-  CU_ASSERT(c->start == ((void*)c+sizeof(chunk)));
-  
+  CU_ASSERT(c->start == ((void*)c+sizeof(chunk)));  
 }
-void testMANUAL_ADDRESS() {
 
+void testMANUAL_ADDRESS() 
+{
   Manual mem = (Manual) iMalloc(1 Mb, MANUAL + ADDRESS);
   CU_ASSERT(mem->free == adress_free);
   CU_ASSERT(mem->avail == avail);
@@ -56,7 +57,6 @@ void testMANUAL_ADDRESS() {
   CU_ASSERT(c->size == (1 Mb - sizeof(chunk)));
   CU_ASSERT(c->free == 1);
   CU_ASSERT(c->start == ((void*)c+sizeof(chunk)));
-
 }
 
 void testGCD_REFCOUNT_DESCENDING()
@@ -95,8 +95,8 @@ void testBALLOC()
   CU_ASSERT(c1->next == d1);
   CU_ASSERT(d1->next == e1);
   //CU_ASSERT(a1->next == b1);
-
 }
+
 void testFREELIST()
 {
   Manual mem = (Manual) iMalloc(1 Kb, MANUAL + DESCENDING_SIZE);
@@ -133,8 +133,8 @@ void testFREELIST()
   CU_ASSERT(list->current->next == NULL);
   CU_ASSERT(list->current->size == 984);
   CU_ASSERT(list->after == NULL);
-
 }
+
 void testREFCOUNT()
 {
   Managed mem = (Managed) iMalloc(1 Mb, REFCOUNT + DESCENDING_SIZE);
@@ -160,9 +160,10 @@ void testREFCOUNT()
 
   CU_ASSERT(list->current == a1);
   CU_ASSERT(a1->next == NULL);
-
 }
-void testAVAIL() {
+
+void testAVAIL() 
+{
   int c = 1 Kb-sizeof(chunk);
   Manual mem = (Manual) iMalloc(1 Kb, MANUAL + ASCENDING_SIZE);
   CU_ASSERT(mem->avail(((Memory) mem) == c));
@@ -177,8 +178,9 @@ void testAVAIL() {
   CU_ASSERT(mem->avail(((Memory) mem) == c-10-sizeof(chunk)-30-sizeof(chunk)));
   mem->free((Memory) mem,a);
   mem->free((Memory) mem,b);
-  CU_ASSERT(mem->avail(((Memory) mem) == c));	    
+  CU_ASSERT(mem->avail(((Memory) mem) == c));
 }
+
 int main() {
     CU_pSuite pSuiteGCD_REFCOUNT_DESCENDING = NULL;
     CU_pSuite pSuiteMANUAL_ADDRESS = NULL;
@@ -236,5 +238,5 @@ int main() {
     CU_basic_run_tests();
     CU_cleanup_registry();
     return CU_get_error();
-    }
+}
 
