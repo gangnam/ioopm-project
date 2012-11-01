5aebb272be7bdbd1eac5e118b54ec1dd289ae71d
Jesper Andersson <juppeduppe@gmail.com>
2012-11-01 11:31:21 +0100

    skrivit kommentarer

--- Garbage.h
+++ Garbage.h
@@ -1,9 +1,14 @@
-#ifndef __manage_h
-#define __manage_h
+#ifndef __garbage_h
+#define __garbage_h
 
 
 
-
+/* 
+Detta är typedallocator funktionen, så den tar in ett gäng flaggor på formen "3*i"
+vilket betyder 3*sizeof(void*) + sizeof(int)
+den skickar sedan storleken den räknat ut till balloc som allokerar så mycket minne.
+sedan returneras Chunk->start för den chunk vi allokerat(alltså chunkens minne).
+*/
 void *typeReader(Memory mem, char *input);
 // Traverserar hepaen och sätter alla objekts markbitar till 0
 void setZero (Chunk c);
@@ -16,7 +21,6 @@ void mf(void *ptr, void *data);
 
 // Frigör alla objekt vars markbitar fortfarande är satta till 0
 void freeObj (Memory mem,Chunk c);
-
 
 #endif
 
