ff093ceae5da519e0524d0c777e6c3a02be0b749
David <david@ubuntu.(none)>
2012-11-06 16:46:24 +0100

    lagt till free

--- freelist.c
+++ freelist.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include "priv_imalloc.h"
 
-/* Letar igenom freelistan efter en chunk som skall tas bort. Markeras till 1 för att sedan länka ur de chunkar som har en 1 markering. Slår sedan ihop tillgängliga lediga utrymmen efter urlänkning. */
+// Ifall vi har två fria chunks bredvid varandra så vill vi slå ihop de två chunksen
+// för att förhindra fragmentering i vår minnesyta. Samt att den ger tillbaka den 
+// ihopslagna chunken ifall den har slagits ihop, anars får vi tillbaka den orörda chunken.
 Chunk combine(Memory mem, Chunk original) {
 
     Chunk c = memToChunk(mem);
@@ -41,6 +43,7 @@ Chunk combine(Memory mem, Chunk original) {
                 while(list) {
                     if (list->current->combined == 1) {
                         list->current->combined = 0;
+			free(flist->first);
                         flist->first = list->after;
                         list = list->after;
                     } else {
@@ -51,8 +54,8 @@ Chunk combine(Memory mem, Chunk original) {
             }
             while (list) {
                 if(list->current->combined == 1) {
-
                     list->current->combined = 0;
+		    free(prev->after);
                     prev->after = list->after;
                     list = list->after;
                 } else {
@@ -66,12 +69,12 @@ Chunk combine(Memory mem, Chunk original) {
         }
     } else {
 
-        return original;
+      return e;
     }
 
 }
 
-/* Returnerar hur mycket plats det finns ledigt i Memory mem */
+// Returnerar hur mycket plats det finns ledigt i Memory mem 
 unsigned int avail(Memory mem) {
 
     Chunk c = memToChunk(mem);
@@ -84,7 +87,9 @@ unsigned int avail(Memory mem) {
     return avail;
 }
 
-/* Lägger pekaren ptr's Chunk i Freelistan, sorterad enligt ascending, dvs minst först och störst sist. */
+// Lägger pekaren ptr's Chunk i Freelistan, sorterad enligt ascending, 
+// dvs minst först och störst sist. Den kollar även ifall ptr's chunk 
+// kan kombineras för att minska fragmentering i vår minnesyta. 
 unsigned int ascending_free(Memory mem, void *ptr) {
 
     Metafreelist flist = memToMeta(mem);
@@ -127,8 +132,9 @@ unsigned int ascending_free(Memory mem, void *ptr) {
     return 0;
 }
 
-/* Lägger pekaren ptr's Chunk i freelistan, sorterad enligt descending. Dvs största chunken skall vara först i listan och minst chunken skall vara sist */
-
+// Lägger pekaren ptr's Chunk i Freelistan, sorterad enligt descending, 
+// dvs störst först och minst sist. Den kollar även ifall ptr's chunk 
+// kan kombineras för att minska fragmentering i vår minnesyta.
 unsigned int descending_free(Memory mem, void *ptr) {
 
     Metafreelist flist = memToMeta(mem);
@@ -171,7 +177,9 @@ unsigned int descending_free(Memory mem, void *ptr) {
     return 0;
 }
 
-/* Lägger pekaren ptr's Chunk i freelistan, sorterad efter adresstorlek */
+// Lägger pekaren ptr's Chunk i Freelistan, sorterad enligt adresserna, 
+// dvs chunken med lägst adress först och högst sist. Den kollar även ifall ptr's chunk 
+// kan kombineras för att minska fragmentering i vår minnesyta.
 unsigned int adress_free(Memory mem, void *ptr) {
 
     Metafreelist flist = memToMeta(mem);
@@ -214,18 +222,20 @@ unsigned int adress_free(Memory mem, void *ptr) {
     return 0;
 }
 
-/*Tar bort en chunk ur freelistan och pekar om*/
+// Länkar ur en chunk ur freelistan
 void RemoveFromFreelist(Memory mem, Chunk c) {
 
     Metafreelist flist = memToMeta(mem);
     Freelist list = flist->first;
 
     if (list->current->start == c->start) {
+      free(flist->first);
         flist->first = list->after;
     } else {
         Freelist prev = list;
         while (list) {
             if(list->current->start == c->start) {
+	      free(prev->after);
                 prev->after = list->after;
             } else {
                 prev = list;
@@ -235,9 +245,9 @@ void RemoveFromFreelist(Memory mem, Chunk c) {
     }
 }
 
-/* Lägger in en chunk i freelistan, chunken är markerad för vilken sorering
-den har och markeringen avgör vilken sortering den får vid insättning i freelistan.
-Dessa är: ascending = 1, descending = 2 och  adress = 4. */
+// Lägger in en chunk i freelistan, chunken är markerad för vilken sorering
+// den har och markeringen avgör vilken sortering den får vid insättning i freelistan.
+// Dessa är: ascending = 1, descending = 2 och  adress = 4. 
 void InsertFreeList(Memory mem, Chunk c) {
 
     Metafreelist flist = memToMeta(mem);
