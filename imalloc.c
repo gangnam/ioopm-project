d8dbd4040ae5413a7494788cf4e4350f957a74ae
Linus <Linus.ostren@gmail.com>
2012-11-06 12:29:01 +0100

    kommentarer, fixar

--- imalloc.c
+++ imalloc.c
@@ -6,11 +6,11 @@
 #include "rc.h"
 #include "Garbage.h"
 #include "rootset.h"
-
+/* Kollar om c finns, om bytes rymms i c och om c är fri */
 static bool fits(Chunk c, int bytes) {
     return c && (c->size >= (bytes+sizeof(chunk)) && c->free);
 }
-
+/* Skapar en ny chunk som ligger på c->start + bytes plats, och sätter den som fri. Tar även bort c från free-listan. Returnerar c->start. */
 void *split(Memory mem, Chunk c, int bytes) {
 
     Chunk temp = (Chunk) (c->start + bytes);
@@ -24,7 +24,6 @@ void *split(Memory mem, Chunk c, int bytes) {
 
     c->free = 0;
     c->size = bytes;
-
     c->next = temp;
 
     InsertFreeList(mem, temp);
@@ -32,20 +31,20 @@ void *split(Memory mem, Chunk c, int bytes) {
     
     return c->start;
 }
-
+/* Letar i free-listan efter en Chunk som rymmer bytes och är fri och returnerar den. Returnerar NULL om den inte hittar någon med plats. */ 
 Chunk getChunk(Memory mem, chunk_size bytes) {
 
     Metafreelist flist = memToMeta(mem);
     Freelist list = flist->first;
     
     for(;list;list=list->after) {
-        if(fits(list->current, bytes=sizeof(chunk))) {
+        if(fits(list->current, bytes)) {
             return list->current;
         }
     }
     return NULL;
 }
-
+/* Allokerar minne inuti mem av storleken bytes. returnerar en void pekare till starten av minne. Returnerar NULL om den ej rymms. */
 void *balloc(Memory mem, chunk_size bytes) {
 
     Chunk c = getChunk(mem, bytes);
@@ -57,7 +56,7 @@ void *balloc(Memory mem, chunk_size bytes) {
         return NULL;
     }
 }
-
+/* Avgör vilken sorts free-funktion som skall användas beroende på flags. */
 Manipulator whatSort (int flags) {
 
     if (flags & ADDRESS) {
@@ -71,40 +70,40 @@ Manipulator whatSort (int flags) {
     }
     return 0;
 }
-
+/* Huvudfunktionen. Returnerar en virtuell heap med storleken memsiz som har olika egenskaper beroende på flags. */
 struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
 
-    if (flags <= 12) {
+  if (flags <= 12) { // Manual
 
         int totalSize = memsiz;
 
-        char *memory = (char*) malloc(totalSize);
-
-        while (totalSize) memory[--totalSize] = 0;
+        char *memory = (char*) malloc(totalSize); // Allokerar hela heapen
+	if (memory == NULL) return NULL;          // Kollar om minnet är slut, returnar NULL om så är fallet.
+        while (totalSize) memory[--totalSize] = 0; // Nollställer minnet 
         
         private_manual *man = (private_manual *) memory;
-        Manual functions = (Manual) (memory+sizeof(private_manual));
+        Manual functions = (Manual) (memory+sizeof(private_manual)); 
 
         man = (private_manual *) memory;
-
-        man->data = (void *) (memory+manMetaSize);
-        man->functions = functions;
-        man->functions->alloc = balloc;
-        man->functions->avail = avail;
-        man->functions->free = whatSort(flags - 8);
-
-        Chunk temp = (Chunk) (memory+manMetaSize);
+	/* Pekar ut vilka funktioner som skall finnas */
+        man->data = (void *) (memory+manMetaSize); // Pekar data till första chunken
+        man->functions = functions; 
+        man->functions->alloc = balloc; 
+        man->functions->avail = avail; 
+        man->functions->free = whatSort(flags - 8); 
+	/* Skapar första chunken och sätter den som fri */
+        Chunk temp = (Chunk) (memory+manMetaSize); 
         temp->start = (memory+manMetaSize+sizeof(chunk));
         temp->size  = (memsiz-manMetaSize-sizeof(chunk));
         temp->next  = NULL;
         temp->free  = 1;
         temp->refcount = 1;
         temp->markbit = 1;
-
+	/* Skapar free-listan */
         Freelist node = (Freelist) malloc(sizeof(freelist));
         node->current = (Chunk) (memory+manMetaSize);
         node->after = NULL;
-
+	/* Skapar free-listans metahuvud */
         Metafreelist head = (Metafreelist) malloc(sizeof(metafreelist));
         head->first = node;
         head->listType = (flags-8);
@@ -114,24 +113,24 @@ struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
 
         return (Memory) (man->functions);
     }
-    else if (flags <= 52) {
+  else if (flags <= 52) { // Managed
 
         int totalSize = memsiz;
-        char *memory = (char*) malloc(totalSize);
-
-        while (totalSize) memory[--totalSize] = 0;
+        char *memory = (char*) malloc(totalSize); // Allokerar hela heapen
+	if (memory == NULL) return NULL;          // Kollar om minnet är slut, returnar NULL om så är fallet.
+        while (totalSize) memory[--totalSize] = 0; // Nollställer minnet
 
         private_managed *mgr = (private_managed *) memory;
         Managed functions = (Managed) (memory+sizeof(private_managed));
 
         mgr = (private_managed *) memory;
-
+	/* Sätter alloc till balloc */
         mgr->data = (void *) (memory+mgrMetaSize);
         mgr->functions = functions;
         mgr->functions->alloc = balloc;
 
         int i;
-
+	/* Väljer vilka funktioner som skall ingå beroende på vad flags är */
         if (flags >= 48) {
             mgr->functions->rc.retain = increaseReferenceCounter;
             mgr->functions->rc.release = decreaseReferenceCounter;
@@ -159,7 +158,7 @@ struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
             mgr->functions->gc.collect = NULL;
             i = flags-16;
         }
-
+	/* Skapar första chunken och sätter den som fri */
         Chunk temp = (Chunk) (memory+mgrMetaSize);
         temp->start = (memory+mgrMetaSize+sizeof(chunk));
         temp->size  = (memsiz-mgrMetaSize-sizeof(chunk));
@@ -167,11 +166,11 @@ struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
         temp->free  = 1;
         temp->refcount = 1;
         temp->markbit = 1;
-
+	/* Skapar free-listan */
         Freelist node = (Freelist) malloc(sizeof(freelist));
         node->current = (Chunk) (memory+mgrMetaSize);
         node->after = NULL;
-
+	/* Skapar free-listans metahuvud */
         Metafreelist head = (Metafreelist) malloc(sizeof(metafreelist));
         head->first = node;
         head->listType = i;
@@ -182,6 +181,6 @@ struct style *iMalloc(unsigned int memsiz, unsigned int flags) {
         return (Memory) (mgr->functions);
     }
     else {
-        return NULL;
+      return NULL;
     }
 }
